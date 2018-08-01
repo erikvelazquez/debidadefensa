5e750705df5c67beffc53417462901692137d2ca
erikvelazquez <erik.velazquezb@hotmail.com>
2018-08-01 18:41:31 -0500

    Se agrega componente de calendario

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -33,6 +33,7 @@ import {MatSidenavModule,
         MatDividerModule,
         MatIconModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar';
 
 @NgModule({
     imports: [
@@ -50,6 +51,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         DebidadefensaAdminModule,
         DebidadefensaAccountModule,
         DebidadefensaEntityModule,
+        CalendarModule.forRoot(),
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
